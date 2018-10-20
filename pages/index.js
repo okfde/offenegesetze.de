1687066b0d8e49d67894f0fc11982b8ea477e8ed
arnese <arne.semsrott@okfn.de>
2018-10-20 16:40:48 +0200

    update index text

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ const IndexPage = ({ items }) => (
     <div className="hero is-primary is-bold">
       <div className="hero-body ">
         <div className="container">
-          <h1 className="title">Wir öffnen das Bundesgesetzblatt.</h1>
+          <h1 className="title">Offene Gesetze für eine offene Gesellschaft</h1>
           <h2 className="subtitle">
-            Wir brechen das Gesetz um dir offene Gesetz zu geben.
+            Wir öffnen das Bundesgesetzblatt
           </h2>
         </div>
       </div>
@@ -24,26 +24,17 @@ const IndexPage = ({ items }) => (
           <br />
           <h2 className="subtitle">Was gibt's hier?</h2>
           <p>
-            Das BGBl ist das Veröffentlichungsblatt (Gazette) der BRD. Es
-            enthält sozusagen alle Änderungen an der aktuelle Gesetzes- und
-            Verordnungslage (= menschen-lesbare Commits/Diffs), die mit
-            Veröffentlichung in Kraft treten. In einer idealen Welt, gibt es
-            kein BGBl, sondern nur Commits auf ein Gesetzes-Repo (Idee von
-            Bundes-Git). Das ist leider gerade unrealistisch. Stattdessen
-            sollten wir das aktuelle Angebot verbessern. Die Phrase "tritt mit
-            Veröffentlichung in Kraft" stammt aus einer analogen Zeit. Früher
-            hieß das: wenn es ausgedruckt und ausgehängt ist. Der Begriff
-            "Veröffentlichung" hat sich aber seitdem stark verändert. Der
-            Bundesanzeigerverlag "veröffentlicht" das BGBl unter bgbl.de zwar im
-            Internet, aber in der schlechtest vorstellbaren Form. Wir erwarten
-            mehr von einer Veröffentlichung, gerade wenn diese den Effekt hat,
-            die Regeln unseres Zusammenlebens – unsere Gesetzeslage – zu
-            verändern. Aktueller Stand der Veröffentlichung Unter bgbl.de stellt
-            der Bundesanzeiger Verlag das BGBl in einer "kostenlosen
-            Bürgerversion" zur Verfügung, allerdings mit Einschränkungen: PDFs
-            sind gesperrt (kein kopieren, kein Drucken) ältere Ausgaben sind nur
-            Bild-PDFs (kein Text) keine Suche (nur im Abonnement) keine
-            Permalinks keine Daten expliziter Urheberrechtshinweis
+            Im Bundesgesetzblatt (BGBl) werden alle Gesetze der Bundesrepublik Deutschland veröffentlicht. Gesetze treten erst in Kraft, wenn
+            sie dort erscheinen. Trotzdem sind die Gesetzblätter nicht offen verfügbar. Der privatisierte Bundesanzeiger-Verlag, hinter dem 
+            Dumont steht, erhebt Urheberrecht auf die Dokumente.
+            
+            Sie sind zwar online auf bgbl.de einzeln einsehbar, aber können nicht gedruckt oder kopiert werden. Sie sind nicht durchsuchbar,
+            der Bundesanzeiger-Verlag verbietet die Weiterverwendung. Wer die Gesetzblätter des Staates nutzen will, muss dem privaten Verlag 
+            Abo-Gebühren zahlen.
+
+            Wir glauben, dass zentrale Dokumente der Demokratie offen für alle bereitstehen müssen. Das Urheberrecht darf der Demokratie 
+            nicht im Wege stehen. Deswegen stellen wir auf dieser Seite sämtliche Bundesgesetzblätter seit 1949 frei, offen und kostenlos zur 
+            Verfügung. 
           </p>
           <PricingTable />
           <br />
