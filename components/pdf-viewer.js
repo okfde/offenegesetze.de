48bfac55d90c0c8b4639865bc07d15f02dfbbcb7
Johannes Filter <hi@jfilter.de>
2018-11-02 15:43:20 +0100

    improve pdf viewer toggling and loading placeholder

--- components/pdf-viewer.js
+++ components/pdf-viewer.js
@@ -5,16 +5,17 @@ import ReactLoading from 'react-loading';
 
 import PageNumber from './page-number';
 
-// not sure if the height is important
+// NB: height.height
 const Loading = height => (
-  <div
-    style={{
-      display: 'flex',
-      justifyContent: 'center',
-      minHeight: height,
-    }}
-  >
-    <ReactLoading type="spin" color="#0069d0" height="20%" width="20%" />
+  <div style={{ height: `${height.height}px` }}>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <ReactLoading type="spin" color="#0069d0" height="20%" width="20%" />
+    </div>
   </div>
 );
 
@@ -22,6 +23,7 @@ class PDFViewer extends React.Component {
   state = {
     numPages: null,
     pageHeight: 500,
+    jumped: false,
   };
 
   componentDidMount() {
@@ -29,14 +31,15 @@ class PDFViewer extends React.Component {
       workerSrc: '/static/pdf.worker.js',
     });
 
-    if (this.props.page != null) {
+    if (this.props.page != null && this.state.jumped === false) {
       // wait until the ID is avaiable
       const inter = setInterval(() => {
-        const hash = '#p' + this.props.page;
+        const hash = `#p${this.props.page}`;
         const elem = document.querySelector(hash);
         if (elem != null) {
           clearInterval(inter);
           elem.scrollIntoView(true);
+          this.setState({ jumped: true });
         }
       }, 1000);
     }
@@ -46,56 +49,62 @@ class PDFViewer extends React.Component {
     this.setState({ numPages });
   };
 
+  renderLoad = height => <Loading height={height} />;
+
   render() {
     const { numPages, pageHeight } = this.state;
     const { document_url, viewPdf, content, q } = this.props;
     return (
       <div>
-        {viewPdf && (
-          <Document
-            file={document_url}
-            onLoadSuccess={this.onDocumentLoadSuccess}
-            loading={<Loading height={pageHeight} />}
-          >
-            {[...Array(numPages).keys()].map(x => (
-              <div key={x}>
-                <PageNumber numPage={x} />
-                <LazyLoad height={pageHeight} once offset={500} resize>
-                  <Page
-                    key={x}
-                    loading={<Loading height={pageHeight} />}
-                    pageNumber={x + 1}
-                    inputRef={ref => {
-                      this.myPage = ref;
-                    }}
-                    onRenderSuccess={() => {
-                      if (pageHeight !== this.myPage.clientHeight) {
-                        this.setState({ pageHeight: this.myPage.clientHeight });
+        {
+          <div style={{ display: viewPdf ? 'block' : 'none' }}>
+            <Document
+              file={document_url}
+              onLoadSuccess={this.onDocumentLoadSuccess}
+              loading={<Loading height={pageHeight} />}
+            >
+              {[...Array(numPages).keys()].map(x => (
+                <div key={x}>
+                  <PageNumber numPage={x} />
+                  <LazyLoad height={pageHeight} once offset={[500, 500]} resize>
+                    <Page
+                      key={x}
+                      loading={this.renderLoad(pageHeight)}
+                      pageNumber={x + 1}
+                      inputRef={ref => {
+                        this.myPage = ref;
+                      }}
+                      onRenderSuccess={() => {
+                        if (pageHeight < this.myPage.clientHeight) {
+                          this.setState({
+                            pageHeight: this.myPage.clientHeight,
+                          });
+                        }
+                      }}
+                      customTextRenderer={textItem =>
+                        textItem.str
+                          .split(q)
+                          .reduce(
+                            (strArray, currentValue, currentIndex) =>
+                              currentIndex === 0
+                                ? [...strArray, currentValue]
+                                : [
+                                    ...strArray,
+                                    <mark key={currentIndex}>{q}</mark>,
+                                    currentValue,
+                                  ],
+                            []
+                          )
                       }
-                    }}
-                    customTextRenderer={textItem =>
-                      textItem.str
-                        .split(q)
-                        .reduce(
-                          (strArray, currentValue, currentIndex) =>
-                            currentIndex === 0
-                              ? [...strArray, currentValue]
-                              : [
-                                  ...strArray,
-                                  <mark key={currentIndex}>{q}</mark>,
-                                  currentValue,
-                                ],
-                          []
-                        )
-                    }
-                  />
-                </LazyLoad>
-              </div>
-            ))}
-          </Document>
-        )}
-        {!viewPdf &&
-          [...Array(numPages).keys()].map(x => (
+                    />
+                  </LazyLoad>
+                </div>
+              ))}
+            </Document>
+          </div>
+        }
+        <div style={{ display: viewPdf ? 'none' : 'block' }}>
+          {[...Array(numPages).keys()].map(x => (
             <div key={x}>
               <PageNumber numPage={x} />
               <div style={{ minHeight: pageHeight, lineHeight: 1.1 }}>
@@ -103,6 +112,7 @@ class PDFViewer extends React.Component {
               </div>
             </div>
           ))}
+        </div>
       </div>
     );
   }
