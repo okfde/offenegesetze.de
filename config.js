95b6ba8a2f9eba5d7a1e03d6a1bf8539f173b197
Johannes Filter <hi@jfilter.de>
2018-11-02 15:52:06 +0100

    fix bgbl typo

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const MIN_YEAR = 1950;
 const MAX_YEAR = 2018;
 
 const dict = {
-  bgbl1: 'BGBL Teil I',
-  bgbl2: 'BGBL Teil II',
+  bgbl1: 'BGBl. Teil I',
+  bgbl2: 'BGBl. Teil II',
 };
 
 export { MIN_YEAR, MAX_YEAR, dict };
