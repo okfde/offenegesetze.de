c2cff2405d5579b32b16a58f3f0175670cfb1e81
Johannes Filter <hi@jfilter.de>
2018-11-23 18:45:24 +0100

    add feeds page

--- pages/feeds.js
+++ pages/feeds.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 import BaseContent from '../components/layout/base-content';
 
@@ -6,14 +7,45 @@ const Feeds = () => (
   <BaseContent>
     <h1 className="title">Feeds</h1>
     <p>
-      Um auf dem Neusten stand zu bleiben, kannst du unsere{' '}
+      Um auf dem Neusten Stand zu bleiben, kannst du unsere{' '}
       <a href="https://de.wikipedia.org/wiki/RSS_(Web-Feed)">RSS-Feeds</a>{' '}
       abbonieren.
-      <ul>
-        <li>Feed 1</li>
-        <li>Feed 2</li>
-        <li>Feed 3</li>
-      </ul>
+    </p>
+    <ul>
+      <li>
+        <a
+          className="highlight"
+          href="https://api.offenegesetze.de/v1/veroeffentlichung/?format=rss"
+        >
+          Alle Veröffentlichungen
+        </a>
+      </li>
+      <li>
+        <a
+          className="highlight"
+          href="https://api.offenegesetze.de/v1/veroeffentlichung/?format=rss&kind=bgbl1"
+        >
+          Nur BGBl. I
+        </a>
+      </li>
+      <li>
+        <a
+          className="highlight"
+          href="https://api.offenegesetze.de/v1/veroeffentlichung/?format=rss&kind=bgbl1"
+        >
+          Nur BGBl. II
+        </a>
+      </li>
+    </ul>
+    <p>
+      Darüber hinaus bieten wir spezialisierte Feeds für Suchergebnisse an.
+      Dafür bitte unsere{' '}
+      <Link href="/suche">
+        <a className="highlight" href="/suche">
+          Suche
+        </a>
+      </Link>{' '}
+      benutzen und dort abbonieren.
     </p>
   </BaseContent>
 );
