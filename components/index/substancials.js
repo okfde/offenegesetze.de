f28dc979195ccb9e7683a08f2d3ab2945c935cb6
arnese <arne.semsrott@okfn.de>
2018-11-23 19:09:55 +0100

    update bezeichnungen gesetze

--- components/index/substancials.js
+++ components/index/substancials.js
@@ -4,56 +4,56 @@ import Link from 'next/link';
 const data = [
   {
     url: '/veroeffentlichung/bgbl1/1949/1/',
-    title: 'Das Grundgesetz',
+    title: 'Das Grundgesetz (1949)',
     imageSrc: '/static/index-preview/gg.png',
     highlight: true,
   },
   {
     url: '/veroeffentlichung/bgbl1/1974/63/#page=1',
-    title: 'Teilweise Legalisierung der Abtreibung',
+    title: 'Teilweise Legalisierung der Abtreibung (1974)',
     imageSrc: '/static/index-preview/gg.png',
   },
   {
     url: '/veroeffentlichung/bgbl1/2017/52/#page=59',
-    title: 'Ehe für alle',
+    title: 'Ehe für alle (2017)',
     imageSrc: '/static/index-preview/gg.png',
     highlight: true,
   },
   {
     url: '/veroeffentlichung/bgbl1/2005/57/#page=2',
-    title: 'Informationsfreiheitsgesetz',
+    title: 'Informationsfreiheitsgesetz (2005)',
     imageSrc: '/static/index-preview/gg.png',
   },
   {
     url: '/veroeffentlichung/bgbl1/1965/51/#page=1',
-    title: 'Urheberrecht',
+    title: 'Urheberrechtsgesetz (1965)',
     imageSrc: '/static/index-preview/gg.png',
     highlight: true,
   },
   {
     url: '/veroeffentlichung/bgbl1/1998/6/#page=24)',
-    title: 'Strafbarkeit von Vergewaltigung in der Ehe',
+    title: 'Abschaffung der Strafbarkeit von Vergewaltigung in der Ehe (1998)',
     imageSrc: '/static/index-preview/gg.png',
   },
   {
     url: '/veroeffentlichung/bgbl1/1972/74/#page=1',
-    title: 'Das Tierschutzgesetz',
+    title: 'Tierschutzgesetz (1972)',
     imageSrc: '/static/index-preview/gg.png',
     highlight: true,
   },
   {
     url: '/veroeffentlichung/bgbl2/1990/35/#page=1',
-    title: 'Wiedervereinigung',
+    title: 'Verträge zur Wiedervereinigung (1990)',
     imageSrc: '/static/index-preview/gg.png',
   },
   {
     url: '/veroeffentlichung/bgbl1/1993/41/#page=1',
-    title: 'Ayslkompromiss: Neufassung des Asylverfahrensgesetzes',
+    title: 'Ayslkompromiss: Neufassung des Asylverfahrensgesetzes (1993)',
     imageSrc: '/static/index-preview/gg.png',
   },
   {
     url: '/veroeffentlichung/bgbl1/1969/52/#page=9',
-    title: 'Paragraph 175: Teilweise Legalisierung von Homosexälität',
+    title: 'Paragraph 175: Teilweise Legalisierung von Homosexualität (1969)',
     imageSrc: '/static/index-preview/gg.png',
   },
 ];
