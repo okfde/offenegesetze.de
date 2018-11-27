11412b38b63913a0352305916ed7410fbf87c5aa
Johannes Filter <hi@jfilter.de>
2018-11-27 14:09:53 +0100

    mock sass

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/__mocks__/styles.js',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styles.js',
   },
 };
