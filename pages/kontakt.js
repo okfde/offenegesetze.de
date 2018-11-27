402098057f524582cb2450b750f81ab8715cd618
Johannes Filter <hi@jfilter.de>
2018-11-27 16:51:25 +0100

    improve kontakt page

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -6,57 +6,59 @@ const Ueber = () => (
   <BaseContent>
     <h1 className="title">Kontakt</h1>
     <p>
-      Offene Gesetze ist ein ehrenamtliches Projekt des gemeinnützigen Open Knowledge Foundation Deutschland e.V.
-      
-  Das Projekt wurde entwickelt von {' '}<a href="https://johannesfilter.com">Johannes Filter</a>, {' '}<a href="https://stefanwehrmeyer.com">Stefan Wehrmeyer</a> und {' '}<a href="https://twitter.com/arnesemsrott">Arne Semsrott</a>. Das Team ist erreichbar unter {' '}
+      Offene Gesetze ist ein ehrenamtliches Projekt des gemeinnützigen Open
+      Knowledge Foundation Deutschland e.V. Das Projekt wurde entwickelt von{' '}
+      <a href="https://johannesfilter.com">Johannes Filter</a>,{' '}
+      <a href="https://stefanwehrmeyer.com">Stefan Wehrmeyer</a> und{' '}
+      <a href="https://twitter.com/arnesemsrott">Arne Semsrott</a>. Das Team ist
+      erreichbar unter{' '}
       <a className="highlight" href="mailto:info@offenegesetze.de">
         info@offenegesetze.de
-      </a>.<br />
+      </a>
+      .<br />
     </p>
     <br />
     <p>
-      Man kann uns auf{' '}
+      Bitte
       <a className="highlight" href="https://twitter.com/offenegesetze">
-        Twitter folgen
+        {' '}
+        folg uns auf Twitter
       </a>{' '}
       und{' '}
       <a className="highlight" href="https://www.facebook.com/offenegesetze/">
-        Facebook liken
-      </a>.
+        like uns auf Facebook
+      </a>{' '}
+      um auf dem Laufendem zu bleiben.
     </p>
     <h2>Impressum</h2>
     <br />
     <dl>
       <dt>Vereinsadresse:</dt>
       <dd>
-        Open Knowledge Foundation Deutschland e.V.<br />
-        Singerstr. 109<br />
-        10179 Berlin<br />
-        Deutschland<br />
+        Open Knowledge Foundation Deutschland e.V.
+        <br />
+        Singerstr. 109
+        <br />
+        10179 Berlin
+        <br />
+        Deutschland
+        <br />
         <br />
       </dd>
     </dl>
     <dl>
-      <dt>Vereinsregister Nr:</dt>
-      <dd>VR 30468 B</dd>
-      <dt>Umsatzsteuernummer:</dt>
-      <dd>DE278022128</dd>
-    </dl>
-    <dl>
-      <dt>E-Mail:</dt>
-      <dd>
-        <a href="mailto:info@okfn.de">info@okfn.de</a> (<a href="https://pgp.mit.edu/pks/lookup?op=get&amp;search=0xFE54F46F21DDAED1">
-          OpenPGP
-        </a>)
-      </dd>
-      <dt>Telefon:</dt>
-      <dd>
-        <a href="tel:+49 30 57703666 2">+49 30 57703666 0</a>
-      </dd>
-      <dt>Fax:</dt>
-      <dd>
-        <a href="fax:+49 30 57703666 9">+49 30 57703666 9</a>
-      </dd>
+      Vereinsregister Nr: VR 30468 B <br />
+      <br />
+      Umsatzsteuernummer: DE278022128 <br />
+      <br />
+      E-Mail: <a href="mailto:info@okfn.de">info@okfn.de</a> (
+      <a href="https://pgp.mit.edu/pks/lookup?op=get&amp;search=0xFE54F46F21DDAED1">
+        OpenPGP
+      </a>
+      )<br /> <br />
+      Telefon: <a href="tel:+49 30 57703666 2">+49 30 57703666 0</a> <br />{' '}
+      <br />
+      Fax: <a href="fax:+49 30 57703666 9">+49 30 57703666 9</a>
     </dl>
   </BaseContent>
 );
