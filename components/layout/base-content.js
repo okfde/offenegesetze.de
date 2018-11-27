cf41fadc1b6004669298347b1c875c74da3b5ff6
Johannes Filter <hi@jfilter.de>
2018-11-27 21:37:06 +0100

    columns only on tablet and above

--- components/layout/base-content.js
+++ components/layout/base-content.js
@@ -5,7 +5,7 @@ import Base from './base';
 const BaseContent = ({ navItems, children, ...rest }) => (
   <Base navItems={navItems} {...rest}>
     <div className="section content">
-      <div className="columns" style={{ padding: '1rem 0' }}>
+      <div className="columns is-tablet" style={{ padding: '1rem 0' }}>
         <div className="column is-12-mobile is-8-desktop is-offset-2-desktop is-three-fifths-tablet is-offset-one-fifth-tablet">
           {children}
         </div>
