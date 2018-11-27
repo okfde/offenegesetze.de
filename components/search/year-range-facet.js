d2a013e1ab5976c60a8d5a8a9e57e3fb5ea65002
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-11-27 14:03:16 +0100

    Use react input range scss for import

--- components/search/year-range-facet.js
+++ components/search/year-range-facet.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import InputRange from 'react-input-range';
 
-import 'react-input-range/lib/css/index.css';
+import 'react-input-range/src/scss/index.scss';
 
 import { PRIMARY_COLOR_DARK, MAX_YEAR, MIN_YEAR } from '../../misc/config';
 
