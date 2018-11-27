3f957266f37388537c8144cd5291a823156e012e
Johannes Filter <hi@jfilter.de>
2018-11-27 22:21:55 +0100

    add dumpy presse

--- pages/presse.js
+++ pages/presse.js
@@ -6,11 +6,8 @@ import BaseContent from '../components/layout/base-content';
 const Presse = () => (
   <BaseContent>
     <h1 className="title">Presse</h1>
-    <p>
-      OffeneGesetze.de ist ein ehrenamtliches, gemeinnütziges Projekt des Open Knowledge Foundation Deutschland e.V. 
-  
-      Wenden Sie sich bei Presseanfragen bitte an Arne Semsrott unter {' '}arne.semsrott@okfn.de oder +49 30 57703666 0. Richten Sie allgemeine Anfragen bitte an {' '}info@offenegesetze.de.
-    </p>
+    <h2>Pressemitteilung vom 05.12.2018</h2>
+    <p>Offene Daten jetzt. Für Rückfragen kontaktieren Sie uns per Telefon.</p>
   </BaseContent>
 );
 
