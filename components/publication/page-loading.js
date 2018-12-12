01c5ad3b8362fbd75e8d4d706dd6e5430c6f5445
Johannes Filter <hi@jfilter.de>
2018-12-12 14:38:47 +0100

    use an IE-11-compliant spinner component

--- components/publication/page-loading.js
+++ components/publication/page-loading.js
@@ -1,37 +1,33 @@
 import React from 'react';
-import ReactLoading from 'react-loading';
 
 import PageNumber from './page-number';
+import { BounceLoader } from 'react-spinners';
+
 import { PRIMARY_COLOR } from '../../misc/config';
 
+// This is quite hacky but the pager loaders need to rendered right away to
+// determine the appropiate height for the anchor tags.
 const renderLoader = maxPages =>
   [...Array(maxPages).keys()].map(x => {
     return <PageLoading page={x + 1} key={x} />;
   });
 
-function PageLoading({ page }) {
-  return (
-    <div>
-      <PageNumber numPage={page} />
-      <div className="dina4">
-        <div className="dina4-ratio">
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}
-          >
-            <ReactLoading
-              type="spin"
-              color={PRIMARY_COLOR}
-              height="20%"
-              width="20%"
-            />
-          </div>
+const PageLoading = ({ page }) => (
+  <div>
+    <PageNumber numPage={page} />
+    <div className="dina4">
+      <div className="dina4-ratio">
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <BounceLoader sizeUnit="rem" size={20} color={PRIMARY_COLOR} />
         </div>
       </div>
     </div>
-  );
-}
+  </div>
+);
 
 export { PageLoading, renderLoader };
