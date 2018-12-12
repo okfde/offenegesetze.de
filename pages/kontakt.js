9de5bf8a35539baddb112add4dab7ee912d30472
Johannes Filter <hi@jfilter.de>
2018-12-12 19:46:48 +0100

    randomize people order

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -2,17 +2,31 @@ import React from 'react';
 
 import BaseContent from '../components/layout/base-content';
 
+const people = [
+  ['Arne Semsrott', 'https://twitter.com/arnesemsrott'],
+  ['Stefan Wehrmeyer', 'https://stefanwehrmeyer.com'],
+  ['Johannes Filter', 'https://johannesfilter.com'],
+];
+
 const Ueber = () => (
   <BaseContent>
     <h1 className="title">Kontakt</h1>
     <p>
       Offene Gesetze ist ein ehrenamtliches Projekt des gemeinnützigen Open
-      Knowledge Foundation Deutschland e.V. Das Projekt wurde entwickelt von{' '}
-      <a href="https://johannesfilter.com">Johannes Filter</a>,{' '}
-      <a href="https://stefanwehrmeyer.com">Stefan Wehrmeyer</a> und{' '}
-      <a href="https://twitter.com/arnesemsrott">Arne Semsrott</a>. Das Team ist
-      erreichbar unter{' '}
-      <a className="highlight" href="mailto:info@offenegesetze.de">
+      Knowledge Foundation Deutschland e.V. Das Projekt wurde entwickelt von:
+      <ul>
+        {people
+          .map(a => ({ sort: Math.random(), value: a }))
+          .sort((a, b) => a.sort - b.sort)
+          .map(a => a.value)
+          .map(x => (
+            <li>
+              <a href={x[1]}>{x[0]}</a>
+            </li>
+          ))}
+      </ul>
+      Das Team ist erreichbar unter{' '}
+      <a className="highlight" href="mailto:info@OffeneGesetze.de">
         info@offenegesetze.de
       </a>
       .<br />
@@ -21,13 +35,13 @@ const Ueber = () => (
     <p>
       Bitte{' '}
       <a className="highlight" href="https://twitter.com/offenegesetze">
-        folg uns auf Twitter
+        folg' uns auf Twitter
       </a>{' '}
       und{' '}
       <a className="highlight" href="https://www.facebook.com/offenegesetze/">
         like uns auf Facebook
-      </a>,{' '}
-      um auf dem Laufendem zu bleiben.
+      </a>
+      , um auf dem Laufendem zu bleiben.
     </p>
     <h2>Impressum</h2>
     <br />
